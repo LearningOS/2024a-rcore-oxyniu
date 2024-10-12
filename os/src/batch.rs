c1317ab1ec9618e3002a89f8dcf8409ed31dd4c4
oplia <oplia@hust.edu.cn>
2024-10-12 11:33:47 +0800

    ch2-qemu9-ra-modified

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -58,36 +58,40 @@ impl AppManager {
         println!("[kernel] num_app = {}", self.num_app);
         for i in 0..self.num_app {
             println!(
-                "[kernel] app_{} [{:#x}, {:#x})",
+                "[kernel] app_{} [{:#x}, {:#x}), total {}B",
                 i,
                 self.app_start[i],
-                self.app_start[i + 1]
+                self.app_start[i + 1],
+                (self.app_start[i + 1] - self.app_start[i])
             );
         }
     }
 
-    unsafe fn load_app(&self, app_id: usize) {
+    fn load_app(&self, app_id: usize) {
         if app_id >= self.num_app {
             println!("All applications completed!");
             use crate::board::QEMUExit;
             crate::board::QEMU_EXIT_HANDLE.exit_success();
         }
         println!("[kernel] Loading app_{}", app_id);
-        // clear app area
-        core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, APP_SIZE_LIMIT).fill(0);
-        let app_src = core::slice::from_raw_parts(
-            self.app_start[app_id] as *const u8,
-            self.app_start[app_id + 1] - self.app_start[app_id],
-        );
-        let app_dst = core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, app_src.len());
-        app_dst.copy_from_slice(app_src);
-        // Memory fence about fetching the instruction memory
-        // It is guaranteed that a subsequent instruction fetch must
-        // observes all previous writes to the instruction memory.
-        // Therefore, fence.i must be executed after we have loaded
-        // the code of the next app into the instruction memory.
-        // See also: riscv non-priv spec chapter 3, 'Zifencei' extension.
-        asm!("fence.i");
+        unsafe {
+            // clear app area
+            core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, APP_SIZE_LIMIT).fill(0);
+            let app_src = core::slice::from_raw_parts(
+                self.app_start[app_id] as *const u8,
+                self.app_start[app_id + 1] - self.app_start[app_id],
+            );
+            let app_dst = core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, app_src.len());
+            app_dst.copy_from_slice(app_src);
+
+            // Memory fence about fetching the instruction memory
+            // It is guaranteed that a subsequent instruction fetch must
+            // observes all previous writes to the instruction memory.
+            // Therefore, fence.i must be executed after we have loaded
+            // the code of the next app into the instruction memory.
+            // See also: riscv non-priv spec chapter 3, 'Zifencei' extension.
+            asm!("fence.i");
+        }
     }
 
     pub fn get_current_app(&self) -> usize {
@@ -134,9 +138,7 @@ pub fn print_app_info() {
 pub fn run_next_app() -> ! {
     let mut app_manager = APP_MANAGER.exclusive_access();
     let current_app = app_manager.get_current_app();
-    unsafe {
-        app_manager.load_app(current_app);
-    }
+    app_manager.load_app(current_app);
     app_manager.move_to_next_app();
     drop(app_manager);
     // before this we have to drop local variables related to resources manually
